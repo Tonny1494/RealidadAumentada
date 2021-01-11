6b3248935dcb3559c53c157d9210cdab583a82dc
Allison Espinoza <alleespi@espol.edu.ec>
2021-01-11 00:54:27 -0500

    diseño favoritos

--- turistView/src/app/favoritos/favoritos.page.ts
+++ turistView/src/app/favoritos/favoritos.page.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { AuthService } from "src/app/services/auth.service";
 import { RestService } from "../services/rest.service"; //importamos nuestro service
 import { ActivatedRoute } from "@angular/router";
-import { AngularFireAuth } from "@angular/fire/auth"
-
+import { AngularFireAuth } from "@angular/fire/auth";
 
 @Component({
   selector: "app-favoritos",
@@ -25,33 +24,34 @@ export class FavoritosPage implements OnInit {
     console.log(this.user);
     this.getFavoritos();
   }
-  
+
   getFavoritos() {
     //llamamos a la funcion getPost de nuestro servicio.
-    return this.AFauth.currentUser.then(auth =>{
+    return this.AFauth.currentUser.then((auth) => {
       this.rest.getFavoritos().subscribe((data) => {
         console.log(data);
         for (var elemento of data.results) {
           let local = elemento["id_local"];
-          let usuario1 = elemento["id_usuario"]
+          let usuario1 = elemento["id_usuario"];
           this.rest.getLocal(local).subscribe((data) => {
             let nombreComercial = data["nombre_comercial"];
             let idLocal = data["id_local"];
             let logo = data["src_logo"];
             let descripcion = data["descripcion"];
             let direccion = data["direccion"];
-            let indice = auth.email.indexOf('@');
-            let usuario = auth.email.substring(0,indice);
-            if (usuario == usuario1){
+            let slogan = data["slogan"];
+            let indice = auth.email.indexOf("@");
+            let usuario = auth.email.substring(0, indice);
+            if (usuario == usuario1) {
               this.locales.push({
                 nombreComercial: nombreComercial,
                 idLocal: idLocal,
                 logo: logo,
                 descripcion: descripcion,
-                direccion: direccion
+                direccion: direccion,
+                slogan: slogan
               });
             }
-            
           });
         }
       });
